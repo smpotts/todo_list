91989ecf42f8f77700d9a433f78e2dee9f83bc2e
spotts685 <spotts685@gmail.com>
2021-08-23 11:58:05 -0600

    moved checked boxes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,9 @@ class HomeController < ApplicationController
     @trash_em = List.where(:completed => true).destroy_all
     redirect_to lists_url, notice: "All marked items were successfully deleted."
   end
+
+  def trash_em_all
+    @trash_em_all = List.destroy_all
+    redirect_to lists_url, notice: "All items were successfully deleted."
+  end
 end
